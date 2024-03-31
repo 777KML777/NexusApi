b071e268794e1a8a7a97d67183939c673d6ad3d8
Kleber Miranda Lima <klebermirandalima@gmail.com>
2024-03-31 16:53:28 -0300

    Adicionando variáveis de ambiente

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,9 +1,29 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { User } from "./Domain/Entities/User/User";
 import { Enterprise } from "./Domain/Entities/Enterprise/EnterpriseEntity";
 import { PublicationEntity } from "./Domain/Entities/Publication/PublicationEntity";
 import { NotificationEntity } from "./Domain/Entities/Notification/NotificationEntity";
+import "dotenv/config";
+
+
+const getDBConfig = (json: any) => {
+    const newObj = {};
+    
+    for (const prop in json) {
+    if (!prop.startsWith("DB_")) {
+    continue;
+    }
+    const newProp = prop.substring(prop.indexOf("_") + 1).toLowerCase();
+    newObj[newProp] = json[prop];
+    }
+    
+    return newObj;
+    };
+    
+    const config = getDBConfig(process.env) as DataSourceOptions;
+    
+    
 
 export const AppDataSource = new DataSource({
     type: "postgres",
