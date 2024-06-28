20e496bc285bb33bc763fd21d514fd14d5fa8c3b
Kleber777 <klebermirandalima@gmail.com>
2024-06-28 20:21:06 -0300

    Created totalAvailableBalance and returning this information to website

--- src/Dto/HomeDto.ts
+++ src/Dto/HomeDto.ts
@@ -1,25 +1,30 @@
-import { RecentPost } from "./RecentPost"
-
 export class HomeDto {
 
     constructor() {
         this.banks = []
-
+        this.totalBalance = 0
+        this.totalAvailableBalance = 0
     }
 
     banks: BankDto[]
     totalBalance: number
+    totalAvailableBalance: number
 
     sumTotalBalance(): void {
         this.banks.forEach((item) => {
-            this.totalBalance += item.balance
+            this.totalBalance += Number(item.balance)
+
+            console.log('isprimindo nessa porra',item.availableBalance, item.balance)
+            this.totalAvailableBalance +=  Boolean(item.availableBalance) == true?
+             Number(item.balance) : 0
         })
     }
 }
 
 
 export class BankDto {
+    id: number
     bankName: string
     balance: number
-
+    availableBalance: boolean
 }
