cb732b6deb1d8d0b8deafbbc2ad1ac978d6811f6
Kleber777 <klebermirandalima@gmail.com>
2024-06-28 20:23:15 -0300

    Creating update bank method and saving the update bank

--- src/controller/BankController.ts
+++ src/controller/BankController.ts
@@ -12,19 +12,71 @@ export const getById = async (
 ) => {
     const data = readRepoFile('bankJson.json');
 
-    console.log("Params", request.query.bankName)
+    console.log("Params", request.query)
 
     let selectedBank = data.banks
         .filter((item) =>
-            item.bankName == request.query.bankName)
+            item.id == request.query.id)
     console.log(selectedBank)
 
     return response.json(selectedBank)
+}
+
+export const createBank = async (
+    request: Request,
+    response: Response) => {
+
+    // criar novo banco 
+    // criar id, validar se já existe o banco 
+    // passar esse create para o BankController 
+    // mexer urgentemente no atualizar banco
+    // converter o balance para number
+
+    // Warn: Passar type operation para não precisar ter um fs fora do readRepoFile? 
+    const fs = require('fs')
+    const data = fs.readFileSync('bankJson.json')
+    let dataJson = JSON.parse(data)
+    dataJson.banks.push(request.body)
+
+    fs.writeFile('bankJson.json'
+        , JSON.stringify(dataJson), (error) => {
+            if (error) throw error;
+            console.log('Saved Success File')
+        }
+    )
+
+    return response.json().statusCode = 200
 }
 
 export const updateBank = async (
     request: Request,
     response: Response) => {
 
+    const data = readRepoFile('bankJson.json');
+
+    console.log("potencial novo banco", request.body)
+
+    let selectedBank = data.banks
+        .find((item) =>
+            item.id == request.body.id)
+    console.log('banco recuperado -> ', selectedBank)
+
+    // There are two ways for update an object in array map() in findIndex function 
+    const updatedList = data.banks.map(b => b.id == selectedBank.id ?
+        { ...b, balance: Number(request.body.balance) } : b
+        // b = request.body as number : b 
+    )
+    
+    data.banks = updatedList;
+
+    const fs = require('fs')
+    fs.writeFile('bankJson.json'
+        , JSON.stringify(data), (error) => {
+            if (error) throw error;
+            console.log('Saved Success File')
+        }
+    )
+
+    return response.json(selectedBank)
 
 }
